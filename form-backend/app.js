387904833b58d9c93b402837a064c36b6c6f605c
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-10-17 17:34:12 +0200

    ok

--- form-backend/app.js
+++ form-backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const dotenv = require('dotenv');
 const cors = require("cors");
+const { body, validationResult } = require("express-validator");
 
 app.use(cors());
 
@@ -24,23 +25,57 @@ db.connect( (error) => {
     }
 })
 
-app.post("/submit", (req, res) => {
-    const { nome, cognome, telefono, email, corsoInteresse } = req.body;
+
+
+app.post("/submit", [
+  body("nome").isString(),
+  body("cognome").isString(),
+  body("telefono").isMobilePhone(),
+  body("email").isEmail(),
+  body("corsoInteresse").isIn(["react", "vuejs", "nodejs", "mongodb"]),
+], (req, res) => {
+  const { nome, cognome, telefono, email, corsoInteresse } = req.body;
   
-    // Esegui una query SQL per inserire i dati nel database
-    const query = "INSERT INTO tabellaAimage (nome, cognome, telefono, email, corsoInteresse) VALUES (?, ?, ?, ?, ?)";
+  const errors = validationResult(req);
   
-    db.query(query, [nome, cognome, telefono, email, corsoInteresse], (error, results) => {
-      if (error) {
-        console.error("Errore nell'inserimento dei dati:", error);
-        res.status(500).json({ message: "Errore nell'inserimento dei dati" });
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  // Verifica se esiste un record con lo stesso nome e cognome
+  const duplicateQuery = "SELECT * FROM tabellaAimage WHERE nome = ? AND cognome = ?";
+  db.query(duplicateQuery, [nome, cognome], (err, results) => {
+    if (err) {
+      console.error("Errore nella verifica dei duplicati:", err);
+      res.status(500).json({ message: "Errore nella verifica dei duplicati" });
+    } else {
+      if (results.length > 0) {
+        const updateQuery = "UPDATE tabellaAimage SET telefono = ?, email = ?, corsoInteresse = ? WHERE nome = ? AND cognome = ?";
+        db.query(updateQuery, [telefono, email, corsoInteresse, nome, cognome], (error, result) => {
+          if (error) {
+            console.error("Errore nell'aggiornamento del record:", error);
+            res.status(500).json({ message: "Errore nell'aggiornamento del record" });
+          } else {
+            console.log("Dati aggiornati con successo");
+            res.status(200).json({ message: "Aggiornamento avvenuto con successo" });
+          }
+        });
       } else {
-        console.log("Dati inseriti con successo");
-        res.status(200).json({ message: "Registrazione avvenuta con successo" });
+        // Nessun duplicato trovato, procedi con l'inserimento dei dati
+        const insertQuery = "INSERT INTO tabellaAimage (nome, cognome, telefono, email, corsoInteresse) VALUES (?, ?, ?, ?, ?)";
+        db.query(insertQuery, [nome, cognome, telefono, email, corsoInteresse], (error, result) => {
+          if (error) {
+            console.error("Errore nell'inserimento dei dati:", error);
+            res.status(500).json({ message: "Errore nell'inserimento dei dati" });
+          } else {
+            console.log("Dati inseriti con successo");
+            res.status(200).json({ message: "Registrazione avvenuta con successo" });
+          }
+        });
       }
-    });
+    }
   });
-  
+});
 
 app.get("/", (req, res)=> {
     res.send("<h1>Home Page<h1>")
