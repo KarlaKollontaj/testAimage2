26c32532350a3ee7b1d8dc803f3431271c785723
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-10-17 15:19:59 +0200

    ok

--- form-backend/app.js
+++ form-backend/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "pswmysql23",
+    database: "testAimage"
+});
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySql connected...")
+    }
+})
 
 app.get("/", (req, res)=> {
     res.send("<h1>Home Page<h1>")
